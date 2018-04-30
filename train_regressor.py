7b3c36f6eff13f542abca3f5337d00f9f88e8f45
Evgeny Blokhin <eb@tilde.pro>
2018-05-01 00:27:30 +0200

    Minor fixing

--- train_regressor.py
+++ train_regressor.py
@@ -141,7 +141,7 @@ def tune_model(data_file):
     parameter_a = results[-1][0]
 
     results = []
-    for parameter_b in range(5, 31):
+    for parameter_b in range(10, 101, 2):
         avg_mae, avg_r2 = estimate_regr_quality(get_regr(a=parameter_a, b=parameter_b), X, y)
         results.append([parameter_b, avg_mae, avg_r2])
         print("%s\t\t\t%s\t\t\t%s" % (parameter_b, avg_mae, avg_r2))
