2fb53ec35cc8aea53c8a74dc352f9f7180ab1c7d
Evgeny Blokhin <eb@tilde.pro>
2019-05-04 16:51:22 +0200

    Add setup script (not for pypi however at this time)

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup
+
+setup(
+    name='mpds_ml_labs',
+    version='0.0.5',
+    author='Evgeny Blokhin',
+    author_email='eb@tilde.pro',
+    license='LGPL-2.1',
+    packages=['mpds_ml_labs'],
+    install_requires=[
+        'mpds_client', 'pycodcif', 'spglib', 'sklearn', 'imblearn', 'progressbar', 'pg8000'
+    ]
+)
